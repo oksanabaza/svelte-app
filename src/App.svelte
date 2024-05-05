49518d744da79765867a70d499157d76c75d9655
oksana.misiura.cld@gmail.com <oksana.misiura.cld@gmail.com>
2024-05-05 21:44:00 +0100

    Add todo func

--- src/App.svelte
+++ src/App.svelte
@@ -1,72 +1,25 @@
- <div class="section box">
-    <div class="field is-horizontal">
-      <div class="field-label is-normal">
-        <label for="todo" class="label">What should I do?</label>
-      </div>
-      <div class="field-body">
-        <div class="field">
-          <p class="control">
-            <input bind:value={todoText} id="todo" class="input" type="text" placeholder="Type something...">
-          </p>
-        </div>
-        <button on:click={addTodo} class="button">Add Todo</button>
-      </div>
-    </div>
-  </div>
-    <div class="section box">
-    <div class="title is-6">Things yet do</div>
-    <table class="table is-fullwidth">
-      <thead>
-        <th>Task</th>
-        <th>Date</th>
-      </thead>
-      <tbody>
-      {#each todoItems as todo}
-  <tr>
-    <td> {todo.text} </td>
-    <td> {todo.date} </td>
-    <button on:click={deleteTodo(todo.id)} class="button">delete</button>
-  </tr>
-{/each}
-      </tbody>
-    </table>
-  </div>
-    <div class="section box">
-    <div class="title is-6">Things done</div>
-    <table id="done-table" class="table is-fullwidth">
-      <thead>
-        <th>Task</th>
-        <th>Date</th>
-        <th></th>
-      </thead>
-      <tbody>
-        {#each doneItems as todo}
-          <tr>
-            <td> {todo.text} </td>
-            <td> {todo.date}</td>
-          </tr>
-        {/each}
-      </tbody>
-    </table>
-  </div>
-  <script>
+<script>
+  import Title from "./Title.svelte"
+  import AddTodoForm from "./AddTodoForm.svelte";
+  import TodoList from "./TodoList.svelte";
   import { v4 as uuidv4 } from 'uuid';
 
-  let doneItems = [];
   let todoText = "";
-  let todoItems = [];  
+  let todoItems = [];
+  let doneItems = [];
 
-   function addTodo() {
+  function addTodo(todoText) {
     const todo = {
-       text: todoText,
-       date: new Date().toLocaleString("en-IE"),
-       id: uuidv4()
+      text: todoText,
+      date: new Date().toLocaleString("en-IE"),
+      id: uuidv4()
     };
     todoItems.push(todo);
     todoItems = [...todoItems];
     todoText = "";
   }
-    function deleteTodo(id) {
+
+  function deleteTodo(id) {
     const found = todoItems.findIndex((todo) => todo.id == id);
     const done = todoItems[found];
     todoItems.splice(found, 1);
@@ -74,5 +27,13 @@
     doneItems.push(done);
     doneItems = [...doneItems];
   }
-    
 </script>
+
+<div class="container">
+  <Title title="Simple Todo List" subTitle="Fun Things to do"/>
+  <div class="section box">
+    <AddTodoForm addTodo="{addTodo}" />
+  </div>
+  <TodoList caption="Items Todo" items="{todoItems}" deleteHandler="{deleteTodo}"/>
+  <TodoList caption="Items Done" items="{doneItems}"/>
+</div>
